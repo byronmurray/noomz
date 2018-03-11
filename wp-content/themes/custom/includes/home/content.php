8e0b0071832fbd02b8a7d8843c1e22a147beab09
vagrant <vagrant@homestead>
2018-03-11 21:17:00 +0000

    new split home page

--- wp-content/themes/custom/includes/home/content.php
+++ wp-content/themes/custom/includes/home/content.php
@@ -1,18 +1,40 @@
 <div class="cover"> <!-- include partials -->
-  <div class="cover__content">
-    <div class="title__sub">Chris Newman</div>
-    <h2 class="title__main">street artist</h2>
 
-      <?php
-          wp_nav_menu( array(
-              'menu'              => 'portfolio',
-              'theme_location'    => 'portfolio',
-              'menu_class'        => 'cover__catergory-list',
-              'container'         =>  false,
-              )
-          );
-      ?>
 
+
+  <div class="catergory-split" style="background: url(http://wordpress-122219-453416.cloudwaysapps.com//wp-content/uploads/2018/03/P10129821000-e1520715261947.jpg)">
+    <div class="catergory-split__content">
+      <h2>Lorem ipsum dolor</h2>
+      <hr>
+      <a href="#">View Work</a>
+    </div>
   </div>
+
+  <div class="catergory-split" style="background: url(http://wordpress-122219-453416.cloudwaysapps.com//wp-content/uploads/2018/03/99517596-5183-3ebf-9b89-fa1a41e3ccd0.jpg)">
+    <div class="catergory-split__content">
+      <h2>Lorem ipsum dolor</h2>
+      <hr>
+      <a href="#">View Work</a>
+    </div>
+  </div>
+
+  <div class="catergory-split" style="background: url(http://wordpress-122219-453416.cloudwaysapps.com//wp-content/uploads/2018/03/2c234c1b-1345-389f-870d-74d8cc953c72.jpg)">
+    <div class="catergory-split__content">
+      <h2>Lorem ipsum dolor</h2>
+      <hr>
+      <a href="#">View Work</a>
+    </div>
+  </div>
+
+  <div class="catergory-split" style="background: url(http://wordpress-122219-453416.cloudwaysapps.com//wp-content/uploads/2018/03/e53ff050-fbcf-3a34-ad24-a41605d95d56.jpg)">
+    <div class="catergory-split__content">
+      <h2>Lorem ipsum dolor</h2>
+      <hr>
+      <a href="#">View Work</a>
+    </div>
+  </div>
+
+
+
 
 </div>
